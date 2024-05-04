d0e4f5beb567195b5ea529912c3a3feb4cf834f6
josuevanier <150824779+josuevanier@users.noreply.github.com>
2024-05-04 11:27:16 -0400

    Stack adt complete

--- src/main/java/org/example/StacksADT.java
+++ src/main/java/org/example/StacksADT.java
@@ -18,4 +18,41 @@ public class StacksADT<Type>{
         top++;
         arrays[top] = value;
     }
+    public Type pop(){
+        if(!isEmpty()){
+            Type elementRetrieved = this.arrays[top];
+            System.out.println("value "  + elementRetrieved + " has been retrieved");
+            top--;
+            return elementRetrieved;
+        }else System.out.println("Stack is empty");
+        return null;
+    }
+
+    public Type peek(){
+        return this.arrays[top];
+    }
+
+    public void display(){
+        System.out.println("Elements of the stack are: ");
+        for(int i = top; i >= 0; i-- ){
+            System.out.println("["+arrays[i] + "]");
+        }
+    }
+
+    public static void main(String[] args) {
+        StacksADT<String> stacksADT = new StacksADT<>(7);
+
+        stacksADT.push("Josue");
+        stacksADT.push("Jo");
+        stacksADT.push("sue");
+        stacksADT.push("ue");
+
+        stacksADT.display();
+        System.out.println();
+        stacksADT.pop();
+        System.out.println();
+
+        stacksADT.display();
+
+    }
 }
