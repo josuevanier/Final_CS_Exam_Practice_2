328f3b90f0ee2517bc3d99e8527d003d53ee7915
josuevanier <150824779+josuevanier@users.noreply.github.com>
2024-05-09 09:53:27 -0400

    Added heap sort and its time complexity

--- src/main/java/org/example/Sortings.java
+++ src/main/java/org/example/Sortings.java
@@ -3,6 +3,38 @@ package org.example;
 import java.lang.reflect.Array;
 import java.util.Arrays;
 class HeapSort{
+    //nlog)n
+    static void heapify(int arr[], int n, int i) {
+        int largest = i; // Initialize largest as root
+        int l = 2 * i + 1; // left = 2*i + 1
+        int r = 2 * i + 2; // right = 2*i + 2
+
+        // If left child is larger than root
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
+
+        // If right child is larger than largest so far
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+
+        // If largest is not root
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            // Recursively heapify the affected sub-tree
+            heapify(arr, n, largest);
+        }
+    }
+
+    // A utility function to print array of size n
+    static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
 
 }
 class MergeSort{
